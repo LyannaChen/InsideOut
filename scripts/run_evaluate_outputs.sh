00fd03d8513fe212e15893ae205bdcd8a796d24c
LyannaChen <lyannachen@g.ucla.edu>
2025-05-23 01:07:53 -0700

    update scripts

--- scripts/run_evaluate_outputs.sh
+++ scripts/run_evaluate_outputs.sh
@@ -1,3 +1,6 @@
 MODEL_NAME="o4-mini"
+python evaluate_outputs.py -m $MODEL_NAME -i ./generated_scripts/chatgpt/generated_scripts.csv -o ./evaluation_results/$MODEL_NAME/chatgpt
+python evaluate_outputs.py -m $MODEL_NAME -i ./generated_scripts/mistral/generated_scripts.csv -o ./evaluation_results/$MODEL_NAME/mistral
 python evaluate_outputs.py -m $MODEL_NAME -i ./generated_scripts/qwen/generated_scripts.csv -o ./evaluation_results/$MODEL_NAME/qwen
-python evaluate_outputs.py -m $MODEL_NAME -i ./generated_scripts/qwen/generated_scripts.csv -o ./evaluation_results/$MODEL_NAME/qwen
+python evaluate_outputs.py -m $MODEL_NAME -i ./generated_scripts/llama/generated_scripts.csv -o ./evaluation_results/$MODEL_NAME/llama
+
